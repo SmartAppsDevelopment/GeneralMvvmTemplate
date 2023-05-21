5c8eb3d7ab55c90a57ee9f7cdcca71e46f08c6c5
Umer Bilal <umeroffice786@gmail.com>
2023-05-21 14:07:07 +0500

    Revert "complete app"
    This reverts commit 57d919f139069e36db1f8e0269a0145426432023.

--- app/src/main/java/com/example/templatesampleapp/helper/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.templatesampleapp.helper
-
-object  Constants {
-    val BASE_URL=""
-}
--- app/src/main/java/com/example/templatesampleapp/helper/MainApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.templatesampleapp.helper
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MainApplication : Application()
